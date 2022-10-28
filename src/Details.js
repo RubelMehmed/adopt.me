2addc7193e69e5eb3acb300c3decaea661615386
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2022-10-28 10:29:34 +0600

    details added

---
+++ src/Details.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import { useParams } from "react-router-dom";
+
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+}
+async componentDidMount() {
+  const res = await fetch(
+    `http://pets-v2.dev-apis.com/pets?id=${this.props.id}`
+  ) ;
+  const json = await res.json();
+  this.setState(Object.assign({loading:false}, json.pets[0]));
+};
+
+render() {
+  if (this.state.loading) {
+    return <h2>loading _</h2>
+  }
+  const { animal, breed, city, state, description, name } = this.state;
+  return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city} , {state}
+        </h2>
+        <p>{description}</p>
+      </div>
+  )
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details id={params.id} />
+}
+export default WrappedDetails;
--- src/Pet.js
+++ src/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
@@ -7,15 +9,17 @@ const Pet = (props) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link href={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        <h2>
+          {`${animal} — ${breed} — ${location}`}
+        </h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
